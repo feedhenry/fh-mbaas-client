251e0b8788ef751c30f53deeb9f6f0e52c3c7e2d
Craig Brookes <cbrookes@redhat.com>
2015-11-05 10:13:31 +0000

    add admin metrics api

---
+++ test/unit/admin/metrics/test_metrics.js
@@ -0,0 +1,24 @@
+var proxyquire = require('proxyquire');
+var assert = require('assert');
+var util = require('util');
+
+module.exports = {
+  "test_get_metrics":function (done){
+    var mocks = {
+      '../../mbaasRequest/mbaasRequest.js': {
+        admin: function(params, cb){
+          assert.ok(params.resourcePath === "/api/metrics");
+          assert.ok(params.method === "GET");
+          return cb(undefined, {});
+        }
+      }
+    };
+
+    var metricsReq = proxyquire('../../../../lib/admin/metrics/metrics', mocks);
+    metricsReq.getMetrics({},
+      function (err,ok){
+        assert.ok(! err, "did not expect an error " + util.inspect(err));
+        done();
+      });
+  }
+};
