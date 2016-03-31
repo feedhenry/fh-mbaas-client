1a751548b780c858e2dc02c52407f046f5714c79
Luigi Zuccarelli <lzuccare@redhat.com>
2016-03-31 15:25:52 +0100

    Updated stats base path

--- test/unit/admin/stats/test_stats.js
+++ test/unit/admin/stats/test_stats.js
@@ -7,9 +7,8 @@ module.exports = {
     var mocks = {
       '../../mbaasRequest/mbaasRequest.js': {
         admin: function(params, cb){
-          assert.ok(params.resourcePath === "/somedomain/someenv/apps/test/stats/history");
+          assert.ok(params.resourcePath === "/stats/history");
           assert.ok(params.method === "POST");
-          assert.ok(params.guid === "test");
           return cb(undefined, {
             _id: "someformid"
           });
@@ -18,10 +17,7 @@ module.exports = {
     };
 
     var statsReq = proxyquire('../../../../lib/admin/stats/stats', mocks);
-    statsReq.history(
-      {environment: "someenv",
-        domain: "somedomain",
-        "guid":"test"},
+    statsReq.history({},
       function (err,ok){
         done();
       });
